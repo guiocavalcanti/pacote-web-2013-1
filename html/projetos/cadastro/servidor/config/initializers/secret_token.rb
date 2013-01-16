035df4aa2b611c774fbb9ac8b5f486dfe4724818
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-01-16 15:39:24 -0300

    Adiciona exemplos e projetos de HTML

---
+++ html/projetos/cadastro/servidor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Servidor::Application.config.secret_token = 'cfd7868d510ad13106e3760a24e3e7888b3f40132abc267bfcf43ad9074df203dfedce80fa0f7f6fa923d1acc16357afa58d63639b02c2c7425c6ae1adb12fac'
